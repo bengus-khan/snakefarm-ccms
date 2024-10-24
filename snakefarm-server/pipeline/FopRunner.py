e3cefa39c9d0d529c31228da95e943ddc3ab3a7b
Benjamin Gus <116837159+bengus-khan@users.noreply.github.com>
2024-10-24 18:06:39 -0400

    Committing progress on pipeline scripts and config files

--- snakefarm-server/pipeline/FopRunner.py
+++ snakefarm-server/pipeline/FopRunner.py
@@ -1,5 +1,10 @@
 #!/usr/local/bin/python3
 
+# DEV NOTES:
+# ---------------------------------------------------------
+# - Run fop --help to confirm correct arguments
+# - FOP config file needs to include all user fonts (along w/ stock fonts) - need to put this in user's system config instead of document config
+
 import argparse
 import configparser
 import json
@@ -8,31 +13,34 @@ import os
 import queue
 import subprocess
 import threading
+import yaml
 
 class FopRunner:
 
-    def init(self): # Define endpoints
+    def __init__(self): # Define endpoints
+        # may change these values to arguments instead of nulls... gotta figure out how this works in context of the larger automation
         self.document_config_file = None
-        self.fop_config_file = None # FOP config file needs to include all user fonts (along with stock fonts) - need to put this in user's system config instead of document config
+        self.fop_config_file = None
 
-    def DocumentConfigParse(self): # syntax within variable defs is probably wrong - fix this soon
-        self.config = json.load(self.document_config_file)
-        self.fo_file = self.config.output_filename + ".fo"
-        self.images_source = self.config.images_source
-        self.output_formats = {"pdf": self.config.pdf, "postscript": self.config.postscript}
+    def DocumentConfigParse(self):
+        if not os.path.exists(self.document_config_file):
+            print(f'Document config file {self.document_config_file} not found.')
+            return
+        with open(self.document_config_file, 'r') as doc_conf:
+            self.config = json.load(doc_conf)
+        self.fo_file = self.config['output_filename'] + '.fo'
+        self.image_root = self.config['image_root']
+        self.output_selected_pdf = self.config['output_format']['pdf']['selected']
+        self.output_selected_postscript = self.config['output_format']['postscript']['selected']
 
     def XslFoToPdf(self):
-        if self.output_formats["pdf"]:
-            self.pdf_filename = self.config.output_filename + ".pdf"
-            try: subprocess.run("fop", [self.fo_file], [self.pdf_filename], "pdf") # Run fop --help to confirm correct arguments
-            except: print("XSL-FO to PDF transformation unsuccessful.") # Placeholder for logger
-    
+        if self.output_selected_pdf == True:
+            self.pdf_filename = self.config['output_filename'] + '.pdf'
+            try: subprocess.run(['fop', self.fo_file, self.pdf_filename, 'pdf'])
+            except: print('XSL-FO to PDF transformation unsuccessful.') # Placeholder for logger
+
     def XslFoToPostScript(self):
-        if self.output_formats["postscript"]:
-            self.postscript_filename = self.config.output_filename + ".ps"
-            try: subprocess.run("fop", [self.fo_file], [self.postscript_filename], "postscript") # run fop --help to confirm correct arguments
-            except: print("XSL-FO to PostScript transformation unsuccessful.") # Placeholder for logger
-    
-    def RunFop(self, XslFoToPdf, XslFoToPostScript): # Idk if I should have this here
-        XslFoToPdf(self)
-        XslFoToPostScript(self)
+        if self.output_selected_postscript == True:
+            self.postscript_filename = self.config['output_filename'] + '.ps'
+            try: subprocess.run(['fop', self.fo_file, self.postscript_filename, 'postscript'])
+            except: print('XSL-FO to PostScript transformation unsuccessful.') # Placeholder for logger
