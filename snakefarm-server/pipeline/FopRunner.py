4140c7db4200b90cc61e4f43757e667be48582fa
Benjamin Gus <116837159+bengus-khan@users.noreply.github.com>
2024-10-27 22:49:15 -0400

    Added semaphore for single-threading fop subprocesses. Also fixed fop subcommand syntax (for windows) and added config file arguments.

--- snakefarm-server/pipeline/FopRunner.py
+++ snakefarm-server/pipeline/FopRunner.py
@@ -2,7 +2,6 @@
 
 # DEV NOTES:
 # ---------------------------------------------------------
-# - Run fop --help to confirm correct arguments
 # - FOP config file needs to include all user fonts (along w/ stock fonts) - need to put this in user's system config instead of document config
 
 import argparse
@@ -18,9 +17,10 @@ import yaml
 class FopRunner:
 
     def __init__(self): # Define endpoints
-        # may change these values to arguments instead of nulls... gotta figure out how this works in context of the larger automation
+        # need to change these values to appropriate arguments instead of nulls... gotta figure out how this works in context of the larger automation
         self.document_config_file = None
         self.fop_config_file = None
+        self.fop_runner_semaphore = threading.Semaphore(1)
 
     def DocumentConfigParse(self):
         if not os.path.exists(self.document_config_file):
@@ -35,12 +35,14 @@ class FopRunner:
 
     def XslFoToPdf(self):
         if self.output_selected_pdf == True:
-            self.pdf_filename = self.config['output_filename'] + '.pdf'
-            try: subprocess.run(['fop', self.fo_file, self.pdf_filename, 'pdf'])
-            except: print('XSL-FO to PDF transformation unsuccessful.') # Placeholder for logger
+            with self.fop_runner_semaphore:
+                self.pdf_filename = self.config['output_filename'] + '.pdf'
+                try: subprocess.run(['fop', '-c', self.fop_config_file, self.fo_file, '-pdf', self.pdf_filename])
+                except: print('XSL-FO to PDF transformation unsuccessful.') # Placeholder for logger
 
     def XslFoToPostScript(self):
         if self.output_selected_postscript == True:
-            self.postscript_filename = self.config['output_filename'] + '.ps'
-            try: subprocess.run(['fop', self.fo_file, self.postscript_filename, 'postscript'])
-            except: print('XSL-FO to PostScript transformation unsuccessful.') # Placeholder for logger
+            with self.fop_runner_semaphore:
+                self.postscript_filename = self.config['output_filename'] + '.ps'
+                try: subprocess.run(['fop', '-c', self.fop_config_file, self.fo_file, '-ps', self.postscript_filename])
+                except: print('XSL-FO to PostScript transformation unsuccessful.') # Placeholder for logger
